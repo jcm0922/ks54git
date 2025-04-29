16ae10645b38117ea369ec4c189b88d1238a3b13
ASUS <ASUS@DESKTOP-T50VO8O>
2025-04-29 14:30:35 +0900

    [팀이름] 프로젝트 초기셋팅

---
+++ ksmybatis/src/main/java/ksmybatis/admin/orders/mapper/OrdersMapper.java
@@ -0,0 +1,22 @@
+package ksmybatis.admin.orders.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrdersMapper {
+	// 주문이력 삭제(상품코드)
+	int removeOrderItemsByProductCode(String productCode);
+	
+	//주문이력 삭제
+	int removeOrdersByCustomerId(String customerId);
+	// 주문상세 삭제
+	int removeOrderItemsByNum(List<String> orderNumList);
+	
+	// 구매이력 번호 조회
+	List<String> getOrdersNumByCustomerId(String customerId);
+	
+	// 판매자가 등록한 상품을 구매한 이력 삭제
+		int removeOderItemsBySellerId(String sellerId);
+}
---
+++ ksmybatis/src/main/java/ksmybatis/admin/product/service/ProductService.java
@@ -0,0 +1,24 @@
+package ksmybatis.admin.product.service;
+
+import java.util.List;
+
+import ksmybatis.admin.product.domain.Product;
+
+	// 상품 목록 조회
+	public interface ProductService {
+		
+		// 상품삭제
+		boolean removeProductByCode(String productCode);
+
+		// 상풍수정
+		void modifyProduct(Product product);
+		
+		// 상품정보조회
+		Product getProductInfoByCode(String productCode);
+		
+		// 상품등록
+		void addProduct(Product product);
+		
+		// 상품 목록 조회
+		List<Product> getProductList();
+	}
